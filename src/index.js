d44cd6dece2bb420507f344c07d58c3c6dca1268
Shape2ee <shape1026@gmail.com>
2023-05-12 15:09:28 +0900

    Database: test api

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: 'https://my-word-book.vercel.app',
+    // origin: 'https://my-word-book.vercel.app',
+    origin: 'http://localhost:5173',
     credentials: true,
   })
 )
