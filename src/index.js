689dfdbacfed2445d527e3558f78d4265c2954c5
Shape2ee <shape1026@gmail.com>
2023-05-12 15:29:32 +0900

    Database: Revert before cors url

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ app.use(express.json())
 
 app.use(
   cors({
-    // origin: 'https://my-word-book.vercel.app',
-    origin: 'http://localhost:5173',
+    origin: 'https://my-word-book.vercel.app',
+    // origin: 'http://localhost:5173',
     credentials: true,
   })
 )
